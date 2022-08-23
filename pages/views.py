672b9cab15a9ece0301c21d946cd95b58f42c58d
Kevonne <KevonneM@gmail.com>
2022-08-22 18:18:31 -0600

    Added templates for other pages along with urls and views.

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 def homepage_view(request):
 
     return render(request, 'home.html')
+
+def launch_event_view(request):
+
+    return render(request, 'launches-events.html')
+
+def iss_location_info_view(request):
+
+    return render(request, 'iss-location-astronauts.html')
+
+def agency_view(request):
+
+    return render(request, 'agencies.html')
