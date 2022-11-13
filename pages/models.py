e9263aab5329fecc1db9c4e67a836f6ca4e575cd
Kevonne <KevonneM@gmail.com>
2022-11-13 01:59:46 -0700

    I added a view that will be scheduled to run daily to update astronauts in database.

--- pages/models.py
+++ pages/models.py
@@ -15,17 +15,21 @@ class UpcomingLaunch(models.Model):
     def __str__(self):
         # shows an actual string representation of the rocket name.
         return self.name
+    class Meta:
+        verbose_name_plural = "Launches"
 
-class Launch(models.Model):
+class Astronaut(models.Model):
 
     name = models.CharField(max_length=75)
-    launch_provider = models.CharField(max_length=75)
-    launch_location = models.CharField(max_length=75)
-    launch_date_time = models.CharField(max_length=75)
-    launch_status = models.CharField(max_length=75)
-    stream_url = models.URLField(blank=True, null=True, max_length=200)
+    nationality = models.CharField(max_length=75)
+    agency = models.CharField(blank=True, null=True, max_length=75)
+    status = models.CharField(max_length=75)
+    bio = models.TextField()
+    date_of_birth = models.DateField(blank=True, null=True)
+    date_of_death = models.DateField(blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
     image = models.ImageField(blank=True, null=True)
-    
+
     def __str__(self):
-        # shows an actual string representation of the rocket name.
+        # shows an actual string representation of the astronauts name.
         return self.name
